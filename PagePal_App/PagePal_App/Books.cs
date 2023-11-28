3a9c7ed9bd64bec4a76126af21d0443f1ba7d48e
Eduardo Rivera <Eduardo.Rivera@pgip-tech.com>
2023-11-28 16:11:26 -0500

    Updated Database Tables

--- PagePal_App/PagePal_App/Books.cs
+++ PagePal_App/PagePal_App/Books.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Text;
 using SQLite;
 
@@ -15,5 +16,41 @@ namespace PagePal_App
         public DateTime yearEntry { get; set; }
         public string Genre { get; set; }
         public string Publication { get; set; }
+
+        [Table("authors")]
+        public class Authors
+        {
+            [PrimaryKey, AutoIncrement]
+            public int AuthorID { get; set; }
+            public string ALastName { get; set; }
+            public string AFirstName { get; set; }
+        }
+
+        [Table("booktable")]
+        public class BookTable
+        {
+            [PrimaryKey, AutoIncrement]
+            public int BookID { get; set; }
+            public string Genre { set; get; }
+            public string BPublication { get; set; }
+        }
+
+        [Table("toread")]
+        public class ToRead
+        {
+            [PrimaryKey, AutoIncrement]
+            public int ToReadID { get; set; }
+        }
+
+        [Table("users")]
+        public class Users
+        {
+            [PrimaryKey, AutoIncrement]
+            public int UserID { get; set; }
+            public string username { get; set; }
+            public string email { get; set; }
+            public string UFirstName { get; set; }
+            public string ULastName { get; set; }
+        }
     }
 }
