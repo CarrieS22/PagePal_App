53542074c3b61c69a93d77f645ff926ff587d668
Carrie Sapp <CarrieRobinson9@kaplanstudent.onmicrosoft.com>
2023-12-16 22:28:19 -0600

    Login Changes

--- PagePal_App/PagePal_App/LoginPage.xaml.cs
+++ PagePal_App/PagePal_App/LoginPage.xaml.cs
@@ -22,21 +22,41 @@ namespace PagePal_App
 
         //public void Button_Clicked(object sender, EventArgs e)
         //{
-            //if(txtUsername.Text == "Username123" && txtPassword.Text == "123456")
-            //{
-                //Navigation.PushAsync(new MainPage());
-            //}
-            //else
-            //{
-                //DisplayAlert("Oops..", "Username/Password incorrect.", "OK");
-            //}
+        //if(txtUsername.Text == "Username123" && txtPassword.Text == "123456")
+        //{
+        //Navigation.PushAsync(new MainPage());
+        //}
+        //else
+        //{
+        //DisplayAlert("Oops..", "Username/Password incorrect.", "OK");
+        //}
         //}
 
         async void Button_Clicked(object sender, EventArgs e)
         {
+            var user = new BookTables.Users
             {
+                UUsername = txtUsername.Text,
+                UPassword = txtPassword.Text
+            };
+            var isValid = AreCredentialsCorrect(user);
+            if (string.IsNullOrEmpty(txtPassword.Text) && string.IsNullOrEmpty(txtUsername.Text))
+            {
+                messageLabel.Text = "Login failed";
+                txtPassword.Text = string.Empty;
+            }
+            else if (isValid)
+            {
+                App.UserName = txtUsername.Text.ToString();
+                App.IsUserLoggedIn = true;
+                Navigation.InsertPageBefore(new MainPage(), this);
+                await Navigation.PopAsync();
+            }
         }
+
+        public bool AreCredentialsCorrect(BookTables.Users user)
         {
+            return txtPassword.Text == user.UUsername && txtPassword.Text == user.UPassword;
         }
 
         void Handle_Tapped(object sender, System.EventArgs e)
